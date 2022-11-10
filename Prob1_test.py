6d5592a1403dc64732116be2f430d860ccbe757a
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-10 12:36:39 +0700

    Update Prob1_test.py

--- Prob1_test.py
+++ Prob1_test.py
@@ -1,6 +1,7 @@
 import Prob1 as p1
 import numpy as np
 
+'''test x'''
 def test_1():
     n = 4
     Aa = [[6,-2,2,4],[12,-8,6,10],[3,-13,9,3],[-6,4,1,-18]]
@@ -10,5 +11,23 @@ def test_1():
     x = p1.Problem1(A,b)
     assert ( np.absolute(x - np.array([ 3.,  1., -2.,  1.]) )<1e-5).all()
 
-
-
+'''test L,U'''
+def test_2():
+    n = 4
+    Aa = [[6,-2,2,4],[12,-8,6,10],[3,-13,9,3],[-6,4,1,-18]]
+    A = np.array(Aa,dtype=np.float64)
+    L,U = p1.LUdecomposition(A)
+    Areconstruct = np.dot(L,U)
+    np.absolute(A-Areconstruct) <1e-8).all()
+    assert np.absolute(A-Areconstruct) <1e-8).all()
+    
+ '''test z'''
+def test_3():
+    n = 4
+    Aa = [[6,-2,2,4],[12,-8,6,10],[3,-13,9,3],[-6,4,1,-18]]
+    bb = [16,26,-19,-34]
+    A = np.array(Aa,dtype=np.float64)
+    b = np.array(bb, dtype=np.float64)    
+    z = Lsubstitution(L,b):
+    zans = np.array([16. -6. -9. -3.])
+    assert ( np.absolute(z - zans )<1e-5).all()
