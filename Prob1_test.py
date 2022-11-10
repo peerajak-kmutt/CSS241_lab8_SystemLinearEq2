8021cd18fa470e08548dccca9c3a039687b1431a
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-10 13:18:41 +0700

    Update Prob1_test.py

--- Prob1_test.py
+++ Prob1_test.py
@@ -21,13 +21,14 @@ def test_2():
     Lans = np.array([[ 1.,   0.,   0.,   0. ], [ 2.,   1.,   0.,   0. ], [ 0.5 , 3.,   1.,   0. ], [-1.,  -0.5,  2.,   1. ]],dtype = np.float64)
     assert (np.absolute(A-Areconstruct) <1e-8).all() and (np.absolute(L-Lans) <1e-8).all()
     
-'''test z'''
+    
 def test_3():
     n = 4
     Aa = [[6,-2,2,4],[12,-8,6,10],[3,-13,9,3],[-6,4,1,-18]]
     bb = [16,26,-19,-34]
     A = np.array(Aa,dtype=np.float64)
-    b = np.array(bb, dtype=np.float64)    
-    z = p1.Lsubstitution(L,b):
+    b = np.array(bb, dtype=np.float64) 
+    L,U = p1.LUdecomposition(A)
+    z = p1.Lsubstitution(L,b)
     zans = np.array([16. -6. -9. -3.])
     assert ( np.absolute(z - zans )<1e-5).all()
