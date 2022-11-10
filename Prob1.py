a751720ad09a0f9a70849addb60dbcbaeb962300
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-10 12:26:28 +0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,17 +1,27 @@
 import numpy as np
-
-def GaussianForwardElimination(A,b): #A,b are numpy array of size (n,n) and n for equation Ax=b
+def LUdecomposition(A):
     m,n = A.shape
     bn, = b.shape
     assert n==m==bn
-    Aans = A.copy()
-    bans = b.copy()
+    U = A.copy()
+    L = np.zeros_like(Aans)
+    np.fill_diagonal(L, 1)
 
+    #Your code here
+    #end Your code
+    print('correct LU = A') if (np.absolute(np.dot(L,U)-A) < 1e-5).all() else print('wrong')
+    return L,U
+
+# Calculae Lz=b
+def Lsubstitution(L,b):
+    m,n = L.shape
+
+    z = np.zeros(n,dtype=np.float64)
     #Your Code Here
     #End Your code
-    
-    return Aans,bans
+    return z
 
+# Calculate Ux = z
 def GaussianBacksubstitution(A,b):
     m,n = A.shape
     bn, = b.shape
@@ -24,6 +34,8 @@ def GaussianBacksubstitution(A,b):
     return x
 
 def Problem1(A,b): 
-    Aelem, belem = GaussianForwardElimination(A,b)
-    x = GaussianBacksubstitution(Aelem,belem)
+    #Your code here.
+    #1. Calculate A=LU. Given A is known, find L,U
+    #2. Calculate Lz=b  Given L from 1., and b from the problem, find z
+    #3. Calculate Ux=z  Given U from 1., and z from 2., find x
     return x
