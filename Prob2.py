091b9bf92c31e69ca3ff181cd82d7763ffe6f9c2
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-10 12:38:35 +0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,31 +1,14 @@
 import numpy as np
 
-def GaussianSwapForwardElimination(A,b):
-    m,n = A.shape
-    bn, = b.shape
-    assert n==m==bn
-    Aans = A.copy()
-    bans = b.copy()
-    l = list(range(n))
-    sl = np.abs(A).max(axis=1)
+def Jacobi(A,b,kmax):
+    n, = b.shape
+    x = np.zeros_like(b)
 
-    #Your Code Here
-    # End your code
-
-    
-    return Aans,bans,l
-
-def GaussianSwapBacksubstitution(A,b,l):
-    m,n = A.shape
-    bn, = b.shape
-    assert n==m==bn
-    x = np.zeros(n,dtype=np.float64)
-    #Your Code Here
-    # End your code
+    #Your code here
+    #end your code
     return x
+            
 
 def Problem2(A,b):
-    Aselem,bselem,l = GaussianSwapForwardElimination(A,b)
-    x = GaussianSwapBacksubstitution(Aselem,bselem,l)
-    return x
+    return Jacobi(A,b,21)
 
